1622c3246d9c15a4cc4436226542132070cf2970
son <ngocsonkt99@gmail.com>
2022-03-05 11:30:21 +0700

    Update_Readme_&&_Refactor_Code

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Nghiên Cứu Flutter
+
+- Nghiên cứu xây dựng giao diện dựa trên các Widget cơ bản
+- Nghiên cứu thoa tác với ListView (LoadList, thêm xóa sửa)
+- Nghiên cứu  thao tác với API (CRUD)
+- Nghiên cứu truyền dữ liệu trong State
--- lib/repositories/ApplicationTracerRepository.dart
+++ lib/repositories/ApplicationTracerRepository.dart
@@ -1,23 +1,23 @@
-import 'dart:convert';
-import 'package:tutorial_flutter/models/ResultModel.dart';
-import 'package:tutorial_flutter/models/ApplicationTracerModel.dart';
-import 'package:tutorial_flutter/services/ApiService.dart';
+// import 'dart:convert';
+// import 'package:tutorial_flutter/models/ResultModel.dart';
+// import 'package:tutorial_flutter/models/ApplicationTracerModel.dart';
+// import 'package:tutorial_flutter/services/ApiService.dart';
 
 
-class ApplicationTracerRepository{
-  // static final baseUrl = '192.168.102.95';
-  static final baseUrl = '172.20.10.2';
+// class ApplicationTracerRepository{
+//   // static final baseUrl = '192.168.102.95';
+//   static final baseUrl = '172.20.10.2';
 
-  static APIService<ApplicationTracerModel> getByNoApli(value){
-    return APIService(
-      url: Uri.http(baseUrl, "/Takaful-API/WebsmartTracer/" + value),
-      parse: (response){
-        final parsed = json.decode(response.body);
-        final dataJson = ResultModel.fromJSON(parsed);
-        var data = ApplicationTracerModel.fromJSON(dataJson.responsedata);
+//   static APIService<ApplicationTracerModel> getByNoApli(value){
+//     return APIService(
+//       url: Uri.http(baseUrl, "/Takaful-API/WebsmartTracer/" + value),
+//       parse: (response){
+//         final parsed = json.decode(response.body);
+//         final dataJson = ResultModel.fromJSON(parsed);
+//         var data = ApplicationTracerModel.fromJSON(dataJson.responsedata);
 
-        return data;
-      }
-    );
-  }
-}
+//         return data;
+//       }
+//     );
+//   }
+// }
